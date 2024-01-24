6c18d02774d4828d5e4cba8b81b598cf55ec6b5b
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2024-01-24 16:00:43 -0300

    getAll and first object

--- backend/index.js
+++ backend/index.js
@@ -4,4 +4,29 @@ const server = express();
 
 server.use(express.json());
 
+const produtos = [
+  {
+    id: 1,
+    nome: "Pizza de Calabresa",
+    valor: 39.9,
+    descricao: "Pizza com molho de tomate, calabresa e cebola.",
+    imagem:
+      "https://guiadacozinha.com.br/wp-content/uploads/2019/10/pizza-liquidificador-receita-768x619.jpg",
+    categoria: "Pizzas",
+  },
+  {
+    id: 2,
+    nome: "Pizza de Frango",
+    valor: 44.9,
+    descricao: "Pizza com molho de tomate, frango e catupiry.",
+    imagem:
+      "https://www.hojetemfrango.com.br/wp-content/uploads/2019/01/shutterstock_333724454.jpg",
+    categoria: "Pizzas",
+  },
+];
+
+server.get("/produtos", (req, res) => {
+  return res.json(produtos);
+});
+
 server.listen(3000);
